d82e410ddc2bcca8142b4c720278af871f652f66
lucasplr <lucasriffel55@gmail.com>
2022-03-22 17:41:40 -0300

    update

--- battleship.js
+++ battleship.js
@@ -13,3 +13,22 @@ let view = {
         cell.setAttribute("class", "miss")
     }
 }
+
+let ships = [{ locations: ["31", "41", "51"], hits: ["", "", ""] },
+{ locations: ["14", "24", "34"], hits: ["", "hit", ""] },
+{ locations: ["00", "01", "02"], hits: ["hit", "", ""] }]
+
+
+var ship2 = ships[1]
+let locations = ship2.locations
+console.log("Locations is " + locations[1])
+
+let ship3 = ships[2]
+let hits = ship3.hits
+if (hits[0] === "hit"){
+    console.log('Ouch, hit on third ship on location one')
+}
+
+let ship1 = ships[0]
+let hits2 = ship1.hits
+hits2[2] = "hit"
