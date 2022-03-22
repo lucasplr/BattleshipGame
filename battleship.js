c5bd5442115c9fae9c0fb30a798b96040b1ba6cc
lucasplr <lucasriffel55@gmail.com>
2022-03-22 16:02:33 -0300

    update

---
+++ battleship.js
@@ -0,0 +1,15 @@
+
+let view = {
+    displayMessage: function(msg){
+        let messageArea = document.getElementById('messageArea')
+        messageArea.innerHTML = msg //this will be setted after the player choose a area.
+    },
+    displayHit: function(location){
+        let cell = document.getElementById(location)
+        cell.setAttribute("class", "hit")
+    },
+    displayMiss: function(location){
+        let cell = document.getElementById(location)
+        cell.setAttribute("class", "miss")
+    }
+}
