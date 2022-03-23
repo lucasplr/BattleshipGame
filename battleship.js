da2ff5a69434df8e4286e5691bc4724f83f1b194
lucasplr <lucasriffel55@gmail.com>
2022-03-23 09:16:07 -0300

    update

--- battleship.js
+++ battleship.js
@@ -14,21 +14,25 @@ let view = {
     }
 }
 
-let ships = [{ locations: ["31", "41", "51"], hits: ["", "", ""] },
-{ locations: ["14", "24", "34"], hits: ["", "hit", ""] },
-{ locations: ["00", "01", "02"], hits: ["hit", "", ""] }]
-
-
-var ship2 = ships[1]
-let locations = ship2.locations
-console.log("Locations is " + locations[1])
-
-let ship3 = ships[2]
-let hits = ship3.hits
-if (hits[0] === "hit"){
-    console.log('Ouch, hit on third ship on location one')
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    fire: function(guess){
+        for (var i = 0; i < this.numShips; i++){
+            var ship = this.ships[i]
+            let locations = ship.locations
+            var index = locations.indexOf(guess)
+            if (index >= 0){
+                console.log('You hit')
+            }else{
+                console.log('Youn dont have a hit')
+            }
+        }
+    }
 }
-
-let ship1 = ships[0]
-let hits2 = ship1.hits
-hits2[2] = "hit"
+model.fire("16")
